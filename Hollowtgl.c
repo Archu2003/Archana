fca2369ee7b1c43dd5f771f125d41a7f7b65908b
Archu2003 <143505681+Archu2003@users.noreply.github.com>
2023-09-12 20:51:36 +0530

    Create Hollowtgl.c

---
+++ Hollowtgl.c
@@ -0,0 +1,29 @@
+include <stdio.h>
+int main()
+{
+    int i,j,n,s;
+    printf("enter the n value:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(s=0;s<n-i-1;s++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<2*i+1;j++)
+        {
+            if(j==0||j==2*i||i==4)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
